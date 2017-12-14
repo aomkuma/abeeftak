76113ca24c632fcf73706f6ece47118862d202f6
aomkuma <korapotu@gmail.com>
2017-12-14 18:25:01 +0700

    Remove js

--- webroot/js/angular-scripts/controllers/HomeController.js
+++
@@ -1,4 +0,0 @@
-angular.module('e-homework').controller('HomeController', function($scope, $cookies, $filter, $state) {
-	//console.log('Hello !');
-    $scope.DEFAULT_LANGUAGE = 'EN';
-});
--- webroot/js/angular-scripts/controllers/ListController.js
+++
@@ -1,4 +0,0 @@
-angular.module('abeef').controller('ListController', function($scope, $cookies, $filter, $state) {
-	//console.log('Hello !');
-    
-});
