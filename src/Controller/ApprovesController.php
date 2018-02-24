02598e729420726d88291fc203578882700ac8ed
sakorn.s <sakorn.s@github.com>
2018-02-24 15:59:02 +0700

    create approve process

---
+++ src/Controller/ApprovesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Approves Controller
+ *
+ *
+ * @method \App\Model\Entity\Approve[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ApprovesController extends AppController {
+    
+    
+    public function cowlist(){
+        
+    }
+    
+    public function farmlist(){
+        
+    }
+    
+    public function herdsmanlist(){
+        
+    }
+    
+    public function cow(){
+        
+    }
+    
+    public function farm(){
+        
+    }
+    
+    public function herdsman(){
+        
+    }
+}
